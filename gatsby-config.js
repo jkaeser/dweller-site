9c41ce687642e78517fdffa439dc60415bd1804c
John Kaeser <jakaeser44@gmail.com>
2018-11-03 21:02:21 -0400

    Configure font styles.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Tailwind',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `abel`,
+          `roboto:400,400i,700,700i`
+        ]
+      }
+    }
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -196,38 +196,20 @@ module.exports = {
 
   fonts: {
     'sans': [
+      'Roboto',
       'system-ui',
       'BlinkMacSystemFont',
       '-apple-system',
       'Segoe UI',
-      'Roboto',
-      'Oxygen',
       'Ubuntu',
       'Cantarell',
-      'Fira Sans',
-      'Droid Sans',
       'Helvetica Neue',
       'sans-serif',
     ],
-    'serif': [
-      'Constantia',
-      'Lucida Bright',
-      'Lucidabright',
-      'Lucida Serif',
-      'Lucida',
-      'DejaVu Serif',
-      'Bitstream Vera Serif',
-      'Liberation Serif',
-      'Georgia',
-      'serif',
-    ],
-    'mono': [
-      'Menlo',
-      'Monaco',
-      'Consolas',
-      'Liberation Mono',
-      'Courier New',
-      'monospace',
+    'header': [
+      'Abel',
+      'Arial',
+      'sans-serif'
     ]
   },
 
