deb80657de93532af4ce9208e7b8185b34823f75
John Kaeser <jakaeser44@gmail.com>
2018-11-03 21:03:03 -0400

    Embed Bandcamp player.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+var style = {
+  border: "none",
+  width: "350px",
+  height: "853px"
+};
+
 const IndexPage = () => (
   <div className="text-center">
+    <iframe style={ style } src="https://bandcamp.com/EmbeddedPlayer/album=4021454801/size=large/bgcol=ffffff/linkcol=0687f5/package=4208657613/transparent=true/" seamless><a href="http://dwellermusic.bandcamp.com/album/dweller-2">Dweller by Dweller</a></iframe>
   </div>
 );
 
