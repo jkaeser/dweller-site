4950c6116c06a2fd18754017187e08ee04435829
John Kaeser <jakaeser44@gmail.com>
2019-05-23 17:01:39 -0400

    Delete old stuff.

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Link from "gatsby-link";
-
-const AboutPage = () => (
-  <div className="flex flex-col md:flex-row items-center">
-  </div>
-);
-
-export default AboutPage;
