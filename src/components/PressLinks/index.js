e7698169ce680cb6c2ae6d700e8af932107c0c56
John Kaeser <jakaeser44@gmail.com>
2020-02-24 21:53:17 -0500

    Make Press links easier to deal with.

---
+++ src/components/PressLinks/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PressLinks = ({ links }) => (
+  <div class="PressLinks">
+    { links.map(link => (
+      <p>
+        <time datetime={ link.date }>{ link.date }: </time>
+        <a href={ link.url }>{ link.url }</a>
+      </p>
+    )) }
+  </div>
+)
+
+export default PressLinks;
--- src/pages/press.js
+++ src/pages/press.js
@@ -2,6 +2,38 @@ import React from "react";
 
 import Layout from "components/Layout";
 import SEO from "components/Seo";
+import PressLinks from "components/PressLinks";
+
+const links = [
+  {
+    date: '12-02-2018',
+    url: 'http://post-trash.com/news/2018/12/2/dweller-bugs-video-post-trash-premiere'
+  },
+  {
+    date: '11-20-2018',
+    url: 'https://reflectivesurfacemag.com/dweller-dweller/'
+  },
+  {
+    date: '10-02-2018',
+    url: 'https://varioussmallflames.co.uk/2018/10/02/dweller-s-t/'
+  },
+  {
+    date: '09-20-2018',
+    url: 'http://www.mindies.es/2018/09/dweller-running/'
+  },
+  {
+    date: '09-12-2018',
+    url: 'http://post-trash.com/news/2018/9/12/dweller-running-post-trash-premiere'
+  },
+  {
+    date: '09-12-2016',
+    url: 'https://www.tristateindie.com/debut-dweller-lp-available-for-streaming-download/'
+  },
+  {
+    date: '07-16-2016',
+    url: 'https://thekey.xpn.org/2016/07/15/reflections-desert-dweller/'
+  }
+]
 
 const PressPage = () => (
   <Layout>
@@ -10,41 +42,7 @@ const PressPage = () => (
       keywords={[`dweller`, `dweller philadelphia`, `philadelphia music`]}
     />
     <h1>Press</h1>
-    <p>
-      <a href="https://varioussmallflames.co.uk/2018/10/02/dweller-s-t/">
-        https://varioussmallflames.co.uk/2018/10/02/dweller-s-t/
-      </a>
-    </p>
-    <p>
-      <a href="http://post-trash.com/news/2018/9/12/dweller-running-post-trash-premiere">
-        http://post-trash.com/news/2018/9/12/dweller-running-post-trash-premiere
-      </a>
-    </p>
-    <p>
-      <a href="http://post-trash.com/news/2018/12/2/dweller-bugs-video-post-trash-premiere">
-        http://post-trash.com/news/2018/12/2/dweller-bugs-video-post-trash-premiere
-      </a>
-    </p>
-    <p>
-      <a href="https://reflectivesurfacemag.com/dweller-dweller/">
-        https://reflectivesurfacemag.com/dweller-dweller/
-      </a>
-    </p>
-    <p>
-      <a href="http://www.mindies.es/2018/09/dweller-running/#tabs-1">
-        http://www.mindies.es/2018/09/dweller-running/#tabs-1
-      </a>
-    </p>
-    <p>
-      <a href="https://www.tristateindie.com/debut-dweller-lp-available-for-streaming-download/">
-        https://www.tristateindie.com/debut-dweller-lp-available-for-streaming-download/
-      </a>
-    </p>
-    <p>
-      <a href="https://thekey.xpn.org/2016/07/15/reflections-desert-dweller/">
-        https://thekey.xpn.org/2016/07/15/reflections-desert-dweller/
-      </a>
-    </p>
+    <PressLinks links={ links } />
   </Layout>
 )
 
