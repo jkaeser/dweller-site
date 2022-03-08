cfcc8705ff006a81ad59a3abf93921a1c5a086d0
John Kaeser <jakaeser44@gmail.com>
2022-03-08 10:24:33 -0500

    Set up initial mobile nav concept.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,6 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import Nav from "components/Nav";
-import NavExternal from "components/Nav/NavExternal";
 
 import './header.css'
 
@@ -36,11 +35,13 @@ const nav_main = [
 const Header = ({ siteTitle }) => (
   <header className="header layout__header">
     <div className="header__inner">
-      <Link to="/" className="logo"><span>Dweller</span></Link>
-      <br />
-      <a href="https://blacklivesmatter.com/" target="_blank" className="blm">
-        <span>Black Lives Matter</span>
-      </a>
+      <div className="header__main">
+        <Link to="/" className="logo"><span>Dweller</span></Link>
+        <br />
+        <a href="https://blacklivesmatter.com/" target="_blank" className="blm">
+          <span>Black Lives Matter</span>
+        </a>
+      </div>
       <Nav items={ nav_main } id={`main`} />
     </div>
   </header>
